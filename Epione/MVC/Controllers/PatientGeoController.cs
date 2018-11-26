288a5ddcce7402543f32807be4471f5fca3f4c84
molka <molka.ouaja@esprit.tn>
2018-11-26 21:10:29 +0100

    molka commit

---
+++ Epione/MVC/Controllers/PatientGeoController.cs
@@ -0,0 +1,108 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class PatientGeoController : Controller
+    {
+        // GET: PatientGeo
+        public ActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:18080");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync("Epione-web/rest/patients").Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+
+                ViewBag.result = response.Content.ReadAsAsync<List<PatientGeoViewModel>>().Result;
+
+
+            }
+            else
+            {
+                ViewBag.result = "error";
+            }
+            return View();
+        }
+
+        // GET: PatientGeo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PatientGeo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PatientGeo/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PatientGeo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PatientGeo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PatientGeo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PatientGeo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Epione/MVC/Models/PatientGeoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class PatientGeoViewModel
+    {
+        public int id { get; set; }
+
+        public string email { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public int phoneNumber { get; set; }
+
+        // public string ville { get; set; }
+
+        // public int codePostal { get; set;  }
+
+        public AdresseViewModel adresse { get; set; }
+    }
+}
